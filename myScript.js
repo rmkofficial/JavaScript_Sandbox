77738a95a018d34a86cf62d4d536736d663c0464
rmkofficial <kmert0@outlook.com>
2023-01-28 12:44:46 +0300

    first commit

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "parapraph changed";
+}
+
+function myFunction1() {
+    document.getElementById("demo").innerHTML = "2nd function worked"
+}
